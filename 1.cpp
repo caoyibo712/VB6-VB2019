ff0ce207803ce42f7dc8f9154f863fc1917027f4
caoyibo712 <64115764+caoyibo712@users.noreply.github.com>
2021-09-10 10:46:44 +0800

    v

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,25 @@
-      1
+struct Xy{
+      double x;
+      double y;
+}
+
+
+
+class vector{
+      public:
+            vector operator+(vector& a,vector& b){
+                  vector v;
+                  v.Xy.x =a.Xy.x +b.Xy.x
+                  v.Xy.y =a.Xy.y +b.Xy.y
+                  return v;
+            }
+            vector operator-(vector& a,vector& b){
+                  vector v;
+                  v.Xy.x =a.Xy.x -b.Xy.x
+                  v.Xy.y =a.Xy.y -b.Xy.y
+                  return v;
+            }
+
+      private:
+            Xy thisxy; 
+}
